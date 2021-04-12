01eb3decce7ad6d8052f3c3a7e7120e16cada8ea
dayadmg <daya.dmg@gmail.com>
2021-04-12 08:22:24 +0000

    first file

---
+++ app.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+ui <- fluidPage(
+  titlePanel(""),
+  sidebarLayout(
+    
+  ),
+  mainPanel(
+    
+  )
+)
+
+server <- function(input, output) {
+  
+  
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui = ui, server = server)
